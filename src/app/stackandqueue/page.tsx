44dc85c4812ea8ad74d0135f443d605026dfd540
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-11 20:13:32 +0530

    fix: fixed issue with build

--- src/app/stackandqueue/page.tsx
+++ src/app/stackandqueue/page.tsx
@@ -1,17 +1,5 @@
 "use client";
 
-import { Flex } from "@chakra-ui/react";
-import AlgorithmView from "../components/AlgorithmView";
-
 export default function Page() {
-    return (
-        <Flex flexDirection="column">
-            <AlgorithmView algorithmName="Stack"></AlgorithmView>
-            <AlgorithmView algorithmName="Queue"></AlgorithmView>
-            <AlgorithmView algorithmName="Insertion Sort"></AlgorithmView>
-            <AlgorithmView algorithmName="Quick Sort"></AlgorithmView>
-            <AlgorithmView algorithmName="Merge Sort"></AlgorithmView>
-            <AlgorithmView algorithmName="Heap Sort"></AlgorithmView>
-        </Flex>
-    );
+    return <div></div>;
 }
