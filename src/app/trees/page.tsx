0f0d0135170999185b096584796c5516e9426ba5
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-12 12:46:22 +0530

    feat: added binary tree and bst

--- src/app/trees/page.tsx
+++ src/app/trees/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { Flex } from "@chakra-ui/react";
+import AlgorithmView from "../views/algorithm/AlgorithmView";
+
 export default function Page() {
-    return <div></div>;
+    return (
+        <Flex flexDirection="column">
+            <AlgorithmView topicName="Binary Tree"></AlgorithmView>
+            <AlgorithmView topicName="Binary Search Tree"></AlgorithmView>
+        </Flex>
+    );
 }
