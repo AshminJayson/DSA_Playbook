e6a0b8a7cde1cbf5c2e1a75335d9dc7a792d8aaa
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-12 14:08:19 +0530

    fix: fixed footer position

--- src/app/views/footer/Footer.tsx
+++ src/app/views/footer/Footer.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { Center, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Footer() {
     return (
-        <Center justifySelf="end" padding="2rem">
+        <Flex justify="center" justifySelf="end" padding="2rem">
             <Text align="center">
                 Made with a ⚡by{" "}
                 <Link href="https://www.linkedin.com/in/ashmin-jayson/">
                     Ashmin Jayson
                 </Link>
             </Text>
-        </Center>
+        </Flex>
     );
 }
